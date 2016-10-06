77045d368a699f0ebe25a3291c9dbe8146aed10e
razielfayad <juankfayad@gmail.com>
2016-10-05 20:35:21 -0500

    hello world en español

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public final class HelloWorld {
  
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("English: Hello World!");
+		System.out.println("Español: ¡Hola Mundo!");
     }
 }
